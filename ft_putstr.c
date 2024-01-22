768cc48d25ab133d1e57e78cb0d95bbf268a988f
Maria Jose Chonata Villamarin <mchonata@c2r7s4.42madrid.com>
2024-01-22 19:17:51 +0100

    putstr ok

--- ft_putstr.c
+++ ft_putstr.c
@@ -1,6 +1,17 @@
-//Si el argumento correspondiente a %s o el campo Buffer del argumento que corresponde a %Z, es un puntero nulo, se mostrará "(null)".
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchonata <mchonata@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/22 16:23:58 by mchonata          #+#    #+#             */
+/*   Updated: 2024/01/22 18:09:10 by mchonata         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "ft_printf.h"
+#define WRITE_NULL "(null)"
 
 size_t	ft_strlen(char *s)
 {
@@ -14,8 +25,10 @@ size_t	ft_strlen(char *s)
 
 int	ft_putstr(char *str)
 {
-	int	len;
+	size_t	len;
 
+	if (!str)
+		return (write (STDOUT_FILENO, WRITE_NULL, ft_strlen(WRITE_NULL)));
 	len = ft_strlen(str);
-	return (write (1, &str, len));
+	return (write (1, str, len));
 }
