b0a6bec3d3727b2dd0d73aed6c0e10296bd0c3f4
Maria Jose Chonata Villamarin <mchonata@c2r14s1.42madrid.com>
2024-02-02 15:55:24 +0100

    printuints opcion 2

--- ft_printuints.c
+++ ft_printuints.c
@@ -6,21 +6,56 @@
 /*   By: mchonata <mchonata@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 13:03:31 by mchonata          #+#    #+#             */
-/*   Updated: 2024/02/02 15:43:16 by mchonata         ###   ########.fr       */
+/*   Updated: 2024/02/02 15:54:18 by mchonata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printuints(unsigned int n)
-{
-	int	aux;
+// int	ft_printuints(unsigned int n)
+// {
+// 	int	aux;
 
-	aux = 0;
+// 	aux = 0;
+// 	if (n > 9)
+// 	{
+// 		aux += ft_printuints(n / 10);
+// 	}
+// 	aux += ft_putchar(n % 10 + '0');
+// 	return (aux);
+// }
+
+static	size_t	size_nbr(unsigned int nbr)
+{
+	size_t	len;
+
+	len = 1;
+	if (nbr < 0)
+	{
+		nbr = nbr * -1;
+		len++;
+	}
+	while (nbr >= 10)
+	{
+		nbr = nbr / 10;
+		len++;
+	}
+	return (len);
+}
+
+static void	ft_putnbr_fd(unsigned int n, int fd)
+{
 	if (n > 9)
 	{
-		aux += ft_printuints(n / 10);
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
 	}
-	aux += ft_putchar(n % 10 + '0');
-	return (aux);
+	else
+		ft_putchar(n + '0');
+}
+
+int	ft_printuints(unsigned int n)
+{
+	ft_putnbr_fd(n, STDOUT_FILENO);
+	return (size_nbr(n));
 }
